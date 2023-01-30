ba06bb82d33a8500de988b6776688cffda0e2578
yamahaxes <bulbenkoevgen@yandex.ru>
2023-01-30 22:57:52 +0700

    feat: add dockerfile

--- stats-client/src/main/java/ru/practicum/ewm/stats/client/StatsClient.java
+++ stats-client/src/main/java/ru/practicum/ewm/stats/client/StatsClient.java
@@ -1,11 +1,8 @@
 package ru.practicum.ewm.stats.client;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.ewm.stats.dto.HitDtoRequest;
 
@@ -15,11 +12,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-@Service
 public class StatsClient extends BaseClient {
 
-    @Autowired
-    public StatsClient(@Value("${stats-server.url}") String serverUrl, RestTemplateBuilder builder) {
+    public StatsClient(String serverUrl, RestTemplateBuilder builder) {
         super(
                 builder
                         .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
